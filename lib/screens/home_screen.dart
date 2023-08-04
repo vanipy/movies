4d4ad8bcde6fd4427258b2ff2103c3157e541328
vanipy <vanessajgarcia.work@gmail.com>
2023-08-04 01:02:43 -0400

    set up 2 screens and video play

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import '../models/movie_model.dart';
+import '../widgets/movie_list_item.dart';
+import 'movie_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Movie> movies = Movie.movies;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: _CustonClipper(),
+          child: Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              color: const Color(0xFF000B49),
+              child: Center(
+                child: Text('Explore',
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+              )),
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 150.0,
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          RichText(
+            text: TextSpan(
+              style: Theme.of(context).textTheme.titleLarge,
+              children: [
+                TextSpan(
+                  text: 'Featured',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                const TextSpan(text: 'Movies'),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          for (final movie in movies)
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MovieScreen(movie: movie),
+                  ),
+                );
+              },
+              child: MovieListItem(
+                imageUrl: movie.imagePath,
+                name: movie.name,
+                information:
+                    '${movie.year}| ${movie.category}|${movie.duration.inHours}h ${movie.duration.inMinutes.remainder(60)}m',
+              ),
+            ),
+        ]),
+      )),
+    );
+  }
+}
+
+class _CustonClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var path = Path();
+
+    path.lineTo(0, height - 50);
+    path.quadraticBezierTo(width / 2, height, width, height - 50);
+    path.lineTo(width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
