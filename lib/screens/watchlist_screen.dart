17d5ae2e9ef77f043d40b615a118ebaef007fa87
vanipy <vanessajgarcia.work@gmail.com>
2023-08-05 14:57:08 -0400

    created a screen for the watchlist

---
+++ lib/screens/watchlist_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WatchlistScreen extends StatefulWidget{
+  const WatchlistScreen({super.key});
+
+  @override 
+  State<WatchlistScreen> createState () => _WatchListScreenState();
+}
+
+class _WatchListScreenState extends State<WatchlistScreen>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Scaffold();
+  }
+}
