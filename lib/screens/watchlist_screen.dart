2f1d6f5fec3cd17faac637893775516078ccad8d
vanipy <vanessajgarcia.work@gmail.com>
2023-08-06 15:55:35 -0400

    updated screens

--- lib/screens/watchlist_screen.dart
+++ lib/screens/watchlist_screen.dart
@@ -1,17 +1,169 @@
+/*CRUD requirements for this watchlist
+Create: be able to add a movie to the watchlist
+Read: be able to display the movie added to the watchlist
+Update: be able to update the list
+Delete: be able to remove a movie from the watchlist
+*/
+
 import 'package:flutter/material.dart';
 
-class WatchlistScreen extends StatefulWidget{
+import '../models/movie_model.dart';
+
+class WatchlistScreen extends StatefulWidget {
   const WatchlistScreen({super.key});
 
-  @override 
-  State<WatchlistScreen> createState () => _WatchListScreenState();
+  @override
+  State<WatchlistScreen> createState() => _WatchListScreenState();
 }
 
-class _WatchListScreenState extends State<WatchlistScreen>
-{
+class _WatchListScreenState extends State<WatchlistScreen> {
+  bool isSwitch = false;
+  bool? isCheckBox = false;
   @override
-  Widget build(BuildContext context)
-  {
-    return const Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 160,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: _CustomClipper(),
+          child: Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xFF000B49),
+            child: Center(
+              child: Text(
+                'Watch List',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+        // title: const Text('Watch List'),
+        // automaticallyImplyLeading: false,
+        // leading: IconButton(
+        //   onPressed: () {
+        //     Navigator.of(context).pop();
+        //   },
+        //   icon: const Icon(
+        //     Icons.arrow_back_ios,
+        //   ),
+        // ),
+        // actions: [
+        //   IconButton(
+        //     onPressed: () {
+        //       debugPrint('actions');
+        //     },
+        //     icon: const Icon(Icons.info_outline),
+        //   ),
+        // ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Image.asset('assets/images/einstein.jpeg'),
+            // const SizedBox(height: 10),
+            // const Divider(
+            //   color: Colors.black,
+            // ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint('Elevated Button');
+              },
+              child: const Text('Elevated Button'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined Button');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text Button');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is a row');
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row Widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckBox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckBox = newBool;
+                  });
+                }),
+            Image.network('https://wallpaperaccess.com/full/1909531.jpg'),
+            Image.network('https://pbs.twimg.com/media/FmxbeaCaMAYAvKG.jpg')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var path = Path();
+
+    path.lineTo(0, height - 50);
+    path.quadraticBezierTo(width / 2, height, width, height - 50);
+    path.lineTo(width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
